124c2f4eb54a923313dc7b36871b6032c06a669f
Luiz <luiz.s08@aluno.ifsc.edu.br>
2023-05-22 22:21:00 -0300

    Final Popup

--- SiteTeste001/script.js
+++ SiteTeste001/script.js
@@ -3,14 +3,14 @@ const popupProfile = document.querySelector('.popup-wrapper')
 const closePopup = document.querySelector('.wrapper')
 
 profileButton.addEventListener('click', ()=> {
-    popupProfile.style.display = 'flex'
+    popupProfile.style.display = 'block'
 })
 
 closePopup.addEventListener('click', event => {
     const classNameOfClickedElement = event.target.classList[0]
     console.log(classNameOfClickedElement)
 
-    if(classNameOfClickedElement === 'closeProfilePopup'||  classNameOfClickedElement === 'btnSettings' || classNameOfClickedElement === 'wrapper' || classNameOfClickedElement === 'undefined') {
+    if(classNameOfClickedElement === 'popup-wrapper' || classNameOfClickedElement === 'closeProfilePopup'||  classNameOfClickedElement === 'btnSettings' || classNameOfClickedElement === 'wrapper' || classNameOfClickedElement === 'undefined') {
         popupProfile.style.display = 'none'
     }
     
