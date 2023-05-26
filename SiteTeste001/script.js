5f6eeb17d41ce29444e03ef078d538ae87ce1599
Luiz <luiz.s08@aluno.ifsc.edu.br>
2023-05-25 22:43:42 -0300

    select 2.0

--- SiteTeste001/script.js
+++ SiteTeste001/script.js
@@ -1,31 +1,52 @@
 //Popup
 const profileButton = document.querySelector('.profileButton')
 const popupProfile = document.querySelector('.popup-wrapper')
-const closePopup = document.querySelector('.wrapper')
-const slider = document.querySelector('.slider')
+const wrapper = document.querySelector('.wrapper')
 
 profileButton.addEventListener('click', ()=> {
     popupProfile.style.display = 'block'
 })
 
-slider.addEventListener('mouseenter', sliderentrar)
-slider.addEventListener('mouseout', slidersair)
-
-function sliderentrar(){
-    
-}
-
-function slidersair(){
-    
-}
-
-closePopup.addEventListener('click', event => {
+wrapper.addEventListener('click', event => {
     const classNameOfClickedElement = event.target.classList[0]
     if(classNameOfClickedElement === 'popup-wrapper' || classNameOfClickedElement === 'closeProfilePopup'||  classNameOfClickedElement === 'btnSettings' || classNameOfClickedElement === 'wrapper' || classNameOfClickedElement === 'undefined') {
         popupProfile.style.display = 'none'
     }
     
 })
+
+//Carrossel
+/*const sliders = document.querySelector('.slider')
+const fundoslider = document.querySelector('.fundopreto-slider')
+const botaoslider = document.querySelector('.manual-navigation')
+
+sliders.addEventListener('mouseenter', sliderentrar)
+sliders.addEventListener('mouseout', slidersair)
+
+function sliderentrar(){
+    fundoslider.style.display = 'block'
+}
+
+function slidersair(){
+    fundoslider.style.display = 'none'
+}*/
+
+let count = 1
+document.getElementById("radio1").checked = true
+
+setInterval(function(){
+    nextImage()
+}, 5000)
+
+function nextImage(){
+    count++
+    if(count>4){
+        count=1
+    }
+
+    document.getElementById("radio"+count).checked = true
+
+}
 
 //Imagem inicial redirecionando para estoque
 var mostra = document.querySelector('.mostrar')
@@ -63,21 +84,4 @@ function sai() {
     imginicial.style.width = '98%'
     mostra.style.textDecoration = 'none'
     mostra.display.transition = '0.2s'
-}
-
-let count = 1
-document.getElementById("radio1").checked = true
-
-setInterval(function(){
-    nextImage()
-}, 5000)
-
-function nextImage(){
-    count++
-    if(count>4){
-        count=1
-    }
-
-    document.getElementById("radio"+count).checked = true
-
 }
